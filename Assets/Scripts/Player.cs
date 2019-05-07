e2a68ad294d50a82d6913c7ef85449521e8949cc
Sinclair <sinfram248@gmail.com>
2019-05-07 11:55:55 +1000

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Prime31;
+
+public class Player : MonoBehaviour {
+
+	public float moveSpeed = 10f;
+	public float gravity = -9.81f;
+
+	private CharacterController2D controller;
+
+    void Start() {
+
+		controller = GetComponent<CharacterController2D>();    
+    }
+
+    void Update() {
+
+		float inputH = Input.GetAxis("Horizontal");
+		float inputV = Input.GetAxis("Vertical");
+
+		controller.move(transform.right * inputH * moveSpeed * Time.deltaTime);
+    }
+}
