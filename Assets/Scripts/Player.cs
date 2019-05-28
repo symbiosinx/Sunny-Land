fe525a122f47ed90427d14d7c9aa94f7593d64f7
Sinclair <sinfram248@gmail.com>
2019-05-28 12:05:20 +1000

    Added animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,18 +9,29 @@ public class Player : MonoBehaviour {
 	public float moveSpeed = 10f;
 	public float gravity = -9.81f;
 
-	private CharacterController2D controller;
+	CharacterController2D controller;
+	Animator anim;
 
     void Start() {
 
-		controller = GetComponent<CharacterController2D>();    
+		controller = GetComponent<CharacterController2D>();
+		anim = GetComponent<Animator>();
     }
 
     void Update() {
 
 		float inputH = Input.GetAxis("Horizontal");
 		float inputV = Input.GetAxis("Vertical");
+		Move(inputH);
+		Climb(inputV);
+	}
 
+	void Move(float inputH) {
 		controller.move(transform.right * inputH * moveSpeed * Time.deltaTime);
-    }
+		anim.SetBool("IsRunning", inputH != 0);
+	}
+
+	void Climb(float inputV) {
+
+	}
 }
